d5be4480f9bdd30c2c542ba674656c73b268f04e
Sara <sarah.alalaya@gmail.com>
2016-06-27 01:11:13 +0300

    Refactoring interactive menu method

--- directory.rb
+++ directory.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 @months = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
+@students = []
 
 def input_students
-  students = []
   puts "Please enter the list of the students"
   puts "To finish, just hit return twice"
   puts "please enter the student's name:"
@@ -19,13 +19,12 @@ def input_students
     end
     cohort = :July  if cohort.empty?
 
-    students << {name: name, cohort: cohort}
-    puts students.count == 1? "Now we have #{students.count} student" : "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort}
+    puts @students.count == 1? "Now we have #{@students.count} student" : "Now we have #{@students.count} students"
 
     puts "please enter the student's name:"
     name = gets.chomp
   end
-  students
 end
 
 
@@ -37,13 +36,13 @@ end
 
 
 
-def print(students)
-  if !students.empty?
+def print_students_list
+  if !@students.empty?
     line_width = 25
     puts "   " +"Name".ljust(line_width) + "Cohort".center(line_width)
     puts
     puts
-    students.each_with_index() do |student, index|
+    @students.each_with_index() do |student, index|
       puts "#{index + 1}. #{student[:name].ljust(line_width)}#{student[:cohort].to_s.center(line_width)}"
     end
   end
@@ -51,41 +50,47 @@ def print(students)
 
 
 
-def print_footer(students)
-  puts students.count == 1? "Overall, we have #{students.count} great student" : "Overall, we have #{students.count} great students"
+def print_footer
+  puts @students.count == 1? "Overall, we have #{@students.count} great student" : "Overall, we have #{@students.count} great students"
 end
 
 
 
 
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+
+
 def interactive_menu
-  students = []
   loop do
-    #step1: print the options (menu, and let the user select one)
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    #step2: Save the user's chois
-    selection = gets.chomp
-
-    #step#: Excute what the user has sellected
-    case selection
-    when "1"
-      students = input_students
-
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-
-    when "9"
-      exit
-
-    else
-      puts "I don't know what you meant, try again"
-
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
