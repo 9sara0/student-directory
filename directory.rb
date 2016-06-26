5535926f93b27f5e2c48ff0e46edc3fd54b6dd0c
Sara <sarah.alalaya@gmail.com>
2016-06-26 13:01:13 +0300

    Exercises(12)

--- directory.rb
+++ directory.rb
@@ -6,32 +6,31 @@ def input_students
   puts "Please enter the list of the students"
   puts "To finish, just hit return twice"
   puts "please enter the student's name:"
-  name = gets.tr("\n","")
+  name = gets.chomp
   cohort = :July # default
 
   while !name.empty? do
     puts "please enter the student's cohort, please type in a full month"
     puts "please be aware: if you did not enter any month then the defualt month is 'July'"
-    cohort = gets.tr("\n","").capitalize.to_sym
+    cohort = gets.chomp.capitalize.to_sym
     while (!@months.include?(cohort)) && (!cohort.empty?) do
     puts "Please make sure that you enterd a correct full month"
-    cohort = gets.tr("\n","").capitalize.to_sym
+    cohort = gets.chomp.capitalize.to_sym
     end
     cohort = :July  if cohort.empty?
 
     puts "please enter the student's hobbies"
-    hobbies = gets.tr("\n","")
+    hobbies = gets.chomp
     puts "please enter the student's place of birth"
-    place_of_birth = gets.tr("\n","")
+    place_of_birth = gets.chomp
     puts "please enter the student's hieght"
-    hieght = gets.tr("\n","")
+    hieght = gets.chomp
 
     students << {name: name, cohort: cohort, hobbies: hobbies, place_of_birth: place_of_birth, hieght: hieght}
     puts students.count == 1? "Now we have #{students.count} student" : "Now we have #{students.count} students"
 
-
     puts "please enter the student's name:"
-    name = gets.tr("\n","")
+    name = gets.chomp
   end
   students
 end
@@ -48,7 +47,7 @@ end
 def slected_first_letter_names(students)
   puts "Please type in a letter to get a list of students whose name begins with that letter."
   puts "Type in 'all' to get the complete list with all student names"
-  intial_letter = gets.tr("\n","")
+  intial_letter = gets.chomp
   if intial_letter.upcase == "ALL" || intial_letter.empty?
     return students
   else
@@ -78,10 +77,10 @@ def cohort_sort(students)
   puts "Please type in a month to get the list of students from that cohort"
   puts "Type in 'all' to get the complete list with all student names"
   puts "Please remmeber if you hit return, you will get the default month cohort 'July'"
-  month = gets.tr("\n","").capitalize.to_sym
+  month = gets.chomp.capitalize.to_sym
   while (!@months.include?(month)) && (!month.empty?) && (month.to_s.upcase != "ALL") do
   puts "Please make sure that you enterd a correct full month"
-  month = gets.tr("\n","").capitalize.to_sym
+  month = gets.chomp.capitalize.to_sym
   end
   month = :July  if month.empty?
 
@@ -95,21 +94,24 @@ end
 
 
 
-
 def print(students)
-  line_width = 25
-  puts "   " +"Name".ljust(line_width) + "Cohort".center(line_width) + "Hobbies".center(line_width) + "Place of Birth".center(line_width) + "Hieght".center(line_width)
-  puts
-  students.each_with_index() do |student, index|
-    puts "#{index + 1}. #{student[:name].ljust(line_width)}#{student[:cohort].to_s.center(line_width)}#{student[:hobbies].center(line_width)}#{student[:place_of_birth].center(line_width)}#{student[:hieght].center(line_width)}"
+  if !students.empty?
+    line_width = 25
+    puts "   " +"Name".ljust(line_width) + "Cohort".center(line_width) + "Hobbies".center(line_width) + "Place of Birth".center(line_width) + "Hieght".center(line_width)
+    puts
+    puts
+    students.each_with_index() do |student, index|
+      puts "#{index + 1}. #{student[:name].ljust(line_width)}#{student[:cohort].to_s.center(line_width)}#{student[:hobbies].center(line_width)}#{student[:place_of_birth].center(line_width)}#{student[:hieght].center(line_width)}"
+    end
   end
-end
+  end
 
 
 
 def print_footer(students)
   puts students.count == 1? "Overall, we have #{students.count} great student" : "Overall, we have #{students.count} great students"
 end
+
 
 students = input_students
 print_header
