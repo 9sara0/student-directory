1922f5eb34da8fc8fde513fc4ced583841a577fd
Sara <sarah.alalaya@gmail.com>
2016-06-26 12:40:49 +0300

    Exercises(2)-fixed

--- directory.rb
+++ directory.rb
@@ -16,29 +16,41 @@ def input_students
 end
 
 
-students = input_students
+
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  puts "Please type in a letter to get a list of students whose name begins with that letter, type in 'all' to get the whole list"
+
+
+def slected_first_letter_names(students)
+  puts "Please type in a letter to get a list of students whose name begins with that letter."
+  puts "Type in 'all' to get the complete list with all student names"
   intial_letter = gets.chomp
-  students.each_with_index() do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if intial_letter.upcase == "all".upcase
+  if intial_letter.upcase == "ALL"
+    return students
+  else
+  names_with_specified_letter = []
+  students.each do |student|
     if student[:name].upcase[0] == intial_letter[0].upcase
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      names_with_specified_letter << student
     end
   end
+      return names_with_specified_letter
+    end
+end
+
+def print(students)
+  students.each_with_index() {|student, index| puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"}
 end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-
+students = input_students
 print_header
-print(students)
+print(slected_first_letter_names(students))
 print_footer(students)
